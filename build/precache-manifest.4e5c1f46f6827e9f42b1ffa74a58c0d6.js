2bb42b12ab2500b7a5149a4a86641ab14cc66b64
SARAVANAN S <sarav18302@gmail.com>
2023-06-10 04:52:48 +0530

    Move folders to root directory

---
+++ build/precache-manifest.4e5c1f46f6827e9f42b1ffa74a58c0d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24aa264f0a82a292eb9d",
+    "url": "/static/css/main.43638695.chunk.css"
+  },
+  {
+    "revision": "24aa264f0a82a292eb9d",
+    "url": "/static/js/main.24aa264f.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "6bacd38d07ed95680c70",
+    "url": "/static/js/2.6bacd38d.chunk.js"
+  },
+  {
+    "revision": "1458782b48af82eb3a96",
+    "url": "/static/js/runtime~main.1458782b.js"
+  },
+  {
+    "revision": "b62eea8f25c9b98503e3c8ca4cd7cf4b",
+    "url": "/index.html"
+  }
+];
